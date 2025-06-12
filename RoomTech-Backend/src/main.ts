1b99a79bb5654870a929b1def281e8bb971b41eb
icawaeo <veronica.waeo@gmail.com>
2025-06-12 09:28:37 +0800

    Enable CORS for Vercel domain

--- RoomTech-Backend/src/main.ts
+++ RoomTech-Backend/src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'framework2-frontend.vercel.app', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
